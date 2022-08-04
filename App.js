64da18021b8e8e7792a7d8443700ed4fde5848d9
gabrielLquadro <gabriel.quadro@tekann.com.br>
2022-08-04 17:40:48 -0300

    adicionado load tela inicial

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect , useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
 
 import api from './src/services/api';
 import Filmes from './src/Filmes';
@@ -7,30 +7,39 @@ import Filmes from './src/Filmes';
 export default function App() {
 
   const [filmes, setFilmes] = useState([]);
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     async function loadFilmes(){
       const response = await api.get('r-api/?api=filmes');
       //console.log(response.data);
       setFilmes(response.data);
+      setLoad(false);
     }
     loadFilmes();
   })
 
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={filmes}
-        renderItem={({item}) => <Filmes data={item}/>}
-        keyExtractor={item => String(item.id)}
-      />
+  if(load){
+    return(
+    <View style={{alignItems:'center', justifyContent:'center', flex:1}}>
+      <ActivityIndicator color='#121212' size={45}/>
     </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 35,
-  },
-});
+    );
+  }else{
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={filmes}
+          renderItem={({item}) => <Filmes data={item}/>}
+          keyExtractor={item => String(item.id)}
+        />
+      </View>
+    );
+  }
+  }
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      marginTop: 35,
+    },
+  });
