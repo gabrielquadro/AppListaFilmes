899b2e1d4fd8822c43797256db6b67b8adc4a7db
gabrielLquadro <gabriel.quadro@tekann.com.br>
2022-08-04 17:20:19 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect , useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+
+import api from './src/services/api';
+import Filmes from './src/Filmes';
 
 export default function App() {
+
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes(){
+      const response = await api.get('r-api/?api=filmes');
+      //console.log(response.data);
+      setFilmes(response.data);
+    }
+    loadFilmes();
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        data={filmes}
+        renderItem={({item}) => <Filmes data={item}/>}
+        keyExtractor={item => String(item.id)}
+      />
     </View>
   );
 }
@@ -13,8 +31,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 35,
   },
 });
